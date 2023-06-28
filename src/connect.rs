9262488c8b6309c8e576df1a2052cffc24731d67
Hart Simha <hmsimha@gmail.com>
2023-06-28 06:51:15 +0000

    Add socks proxy capabilities

--- src/connect.rs
+++ src/connect.rs
@@ -2,10 +2,12 @@
 use tokio::net::TcpStream;
 
 use tungstenite::{
-    error::{Error, UrlError},
+    error::{Error, UrlError, ProtocolError},
     handshake::client::{Request, Response},
     protocol::WebSocketConfig,
 };
+
+use async_socks5;
 
 use crate::{domain, stream::MaybeTlsStream, Connector, IntoClientRequest, WebSocketStream};
 
@@ -17,6 +19,17 @@ where
     R: IntoClientRequest + Unpin,
 {
     connect_async_with_config(request, None, false).await
+}
+
+/// Supply a proxy url and open TCPStream to it; use that stream to connect to the given URL.
+pub async fn connect_async_with_socks_proxy<R>(
+    request: R,
+    socks_proxy: R,
+) -> Result<(WebSocketStream<MaybeTlsStream<TcpStream>>, Response), Error>
+where
+    R: IntoClientRequest + Unpin,
+{
+    connect_async_with_config_with_socks_proxy(request, None, false, socks_proxy).await
 }
 
 /// The same as `connect_async()` but the one can specify a websocket configuration.
@@ -32,6 +45,19 @@ where
     R: IntoClientRequest + Unpin,
 {
     connect(request.into_client_request()?, config, disable_nagle, None).await
+}
+
+/// The same as `connect_async_with_config()` but this also takes a socks proxy url.
+pub async fn connect_async_with_config_with_socks_proxy<R>(
+    request: R,
+    config: Option<WebSocketConfig>,
+    disable_nagle: bool,
+    socks_proxy: R,
+) -> Result<(WebSocketStream<MaybeTlsStream<TcpStream>>, Response), Error>
+where
+    R: IntoClientRequest + Unpin,
+{
+    connect_with_socks_proxy(request.into_client_request()?, config, disable_nagle, None, socks_proxy.into_client_request()?).await
 }
 
 /// The same as `connect_async()` but the one can specify a websocket configuration,
@@ -77,4 +103,51 @@ async fn connect(
     }
 
     crate::tls::client_async_tls_with_config(request, socket, config, connector).await
+}
+
+async fn connect_with_socks_proxy(
+    request: Request,
+    config: Option<WebSocketConfig>,
+    disable_nagle: bool,
+    connector: Option<Connector>,
+    socks_proxy: Request,
+) -> Result<(WebSocketStream<MaybeTlsStream<TcpStream>>, Response), Error> {
+    let proxy_domain = domain(&socks_proxy)?;
+    match socks_proxy.uri().scheme_str() {
+        Some("socks5") | None => Ok(String::from("socks5")),
+        Some("socks5h") => {
+            // possibly print a warning here
+            Ok(String::from("socks5"))
+        },
+        _ => Err(Error::Url(UrlError::UnsupportedUrlScheme)),
+    }?;
+    let proxy_port = socks_proxy
+        .uri()
+        .port_u16()
+        .ok_or(Error::Url(UrlError::UnableToConnect(format!("{proxy_domain}: Port required in socks proxy string"))))?;
+
+    let destination_domain = domain(&request)?;
+    let destination_port = request
+        .uri()
+        .port_u16()
+        .or_else(|| match request.uri().scheme_str() {
+            Some("wss") => Some(443),
+            Some("ws") => Some(80),
+            _ => None,
+        })
+        .ok_or(Error::Url(UrlError::UnsupportedUrlScheme))?;
+
+    let destination_addr = (destination_domain, destination_port);
+
+    let mut proxy_socket = TcpStream::connect(format!("{proxy_domain}:{proxy_port}")).await.map_err(Error::Io)?;
+    async_socks5::connect(&mut proxy_socket, destination_addr, None).await.map_err(|err| match err {
+        async_socks5::Error::Io(msg) => Error::Io(msg),
+        async_socks5::Error::WrongVersion => Error::Protocol(ProtocolError::WrongHttpVersion),
+        _ => Error::Protocol(ProtocolError::JunkAfterRequest)
+    })?;
+    if disable_nagle {
+        proxy_socket.set_nodelay(true)?;
+    }
+
+    crate::tls::client_async_tls_with_config(request, proxy_socket, config, connector).await
 }
--- src/lib.rs
+++ src/lib.rs
@@ -55,7 +55,7 @@ pub use tls::Connector;
 pub use tls::{client_async_tls, client_async_tls_with_config};
 
 #[cfg(feature = "connect")]
-pub use connect::{connect_async, connect_async_with_config};
+pub use connect::{connect_async, connect_async_with_config, connect_async_with_config_with_socks_proxy, connect_async_with_socks_proxy};
 
 #[cfg(all(any(feature = "native-tls", feature = "__rustls-tls"), feature = "connect"))]
 pub use connect::connect_async_tls_with_config;
